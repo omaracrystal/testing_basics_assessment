384b2f03f52c571e4219f14dc43e111bebd1ff0f
Crystal <crystal@Zachs-MacBook-Air.local>
2015-11-03 01:46:57 -0700

    added function modeScore

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-// add scripts
+
 (function letterGrader (score) {
+
+  'use strict';
+
   if (score > 96) {
     return "A+";
   } else if (score > 92) {
@@ -30,8 +33,10 @@
 })();
 
 
-
 (function averageScore (scoreArr){
+
+  'use strict';
+
   var total = 0;
 
   for (var i = 0; i < scoreArr.length; i++) {
@@ -42,8 +47,10 @@
 })();
 
 
-
 (function medianScore (scoreArr) {
+
+  'use strict';
+
   //sort array of numbers from lowest to highest
   var sortArr = scoreArr.sort(function(a,b) {
     return a-b;
@@ -51,8 +58,8 @@
   //check to see if array length is even or odd
   if (sortArr.length  % 2 === 0) {
     //divide array into two even arrays
-    var left = sortArray.splice(0,array.length/2);
-    var right = sortArray;
+    var left = sortArr.splice(0,array.length/2);
+    var right = sortArr;
     var middleAvg = (left[left.length-1]+right[0])/2;
     //return middle average
     return middleAvg;
@@ -60,4 +67,31 @@
     //return middle
     return sortArr[Math.floor((sortArr.length -1)/ 2)];
   }
+})();
+
+
+(function modeScore (scoreArr) {
+
+  'use strict';
+
+  var count = {};
+  var max = 0;
+  var result = [];
+  //iterate through array and count up the occurances of each element
+  scoreArr.forEach(function (i) {
+      count[i] = (count[i] || 0) + 1;
+      //if element has most occurances assign value of max
+      if (count[i] > max) {
+          max = count[i];
+      }
+  });
+  //iterate through object count and pushing max into result array if there are more than two max occurances both will be pushed
+  Object.keys(count).forEach(function (k) {
+      count[k] === max && result.push(k);
+  });
+
+  //converting results from an array of strings to integers
+  var mode = result.map(Number);
+  return mode;
+
 })();
